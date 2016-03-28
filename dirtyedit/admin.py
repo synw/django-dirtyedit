396aa7e50d16c095469a843ebc2ef889ffa62c36
syn <synwe@yahoo.fr>
2016-03-28 17:40:25 +0000

    Better codemirror field

--- dirtyedit/admin.py
+++ dirtyedit/admin.py
@@ -6,22 +6,33 @@ from django.contrib import admin
 from django.contrib import messages
 from codemirror2.widgets import CodeMirrorEditor
 from dirtyedit.models import FileToEdit
+from dirtyedit.forms import FileEditForm
 
 
 USE_REVERSION=getattr(settings, 'USE_REVERSION', True)
 if USE_REVERSION:
     from reversion.admin import VersionAdmin
-
+    
 admin_class=admin.ModelAdmin
 if USE_REVERSION:
     admin_class=VersionAdmin
 @admin.register(FileToEdit)
 class FileToEdit(admin_class):
+    form = FileEditForm
+    save_on_top = True
     
+    """
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.attname == "content":
             kwargs['widget'] = CodeMirrorEditor(options={'mode': 'htmlmixed','indentWithTabs':'true','lineNumbers':'true'}, modes=['css', 'xml', 'javascript', 'htmlmixed'])
         return super(FileToEdit, self).formfield_for_dbfield(db_field, **kwargs)
+    """
+    
+    fieldsets = (
+            (None, {
+                'fields': ('content', 'location')
+            }),
+            )
     
     def save_model(self, request, obj, form, change):
         #~ record editor
