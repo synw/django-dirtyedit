b3fb239fa59544430706956706aa5cee2c2de884
syn <synwe@yahoo.fr>
2016-05-28 14:51:30 +0000

    Setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+version = __import__('dirtyedit').__version__
+
+setup(
+  name = 'django-dirtyedit',
+  packages=find_packages(),
+  include_package_data=True,
+  version = version,
+  description = 'File editor for Django',
+  author = 'synw',
+  author_email = 'synwe@yahoo.com',
+  url = 'https://github.com/synw/django-dirtyedit', 
+  download_url = 'https://github.com/synw/django-dirtyedit/releases/tag/'+version, 
+  keywords = ['django', 'editor'], 
+  classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+  install_requires=[
+        "Django",
+    ],
+  zip_safe=False
+)
